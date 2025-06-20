ab16a20f3883f90ca12fb224c6a9cc9bf5d5af87
NazaretsInfinity <157612924+NazaretsInfinity@users.noreply.github.com>
2025-06-20 18:57:42 +0500

    edited restaurant

--- ASPrazorpages/Pages/About.cshtml.cs
+++ ASPrazorpages/Pages/About.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ASPrazorpages.Pages.Shared
+namespace ASPrazorpages.Pages
 {
-    public class aboutModel : PageModel
+    public class AboutModel : PageModel
     {
         public void OnGet()
         {
---
+++ ASPrazorpages/Pages/RestaurantAddresses.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPrazorpages.Pages
+{
+    public class RestaurantAddressesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
